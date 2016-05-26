dfe1c3b19d1993e933232d6be12fb32f2e68d8e5
Darren Merrill <darren_merrill@hotmail.com>
2016-05-26 13:24:13 -0600

    Add project files.

---
+++ Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Solarizr10.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Solarizr10.Views
+{
+  public sealed partial class DetailPage : Page
+  {
+    public DetailPage()
+    {
+      InitializeComponent();
+      NavigationCacheMode = NavigationCacheMode.Disabled;
+    }
+  }
+}
+
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+using Solarizr10.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Solarizr10.Views
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      NavigationCacheMode = NavigationCacheMode.Enabled;
+    }
+  }
+}
